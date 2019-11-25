745f5eff69f738ef85f64f5a5bec57bf5bc6ade4
c-atanub <atanu.baidya@ext-crisil.com>
2019-11-25 08:02:48 +0530

    Custom language initial commit

---
+++ src/main/java/com/lang/blang/BlangApplication.java
@@ -0,0 +1,13 @@
+package com.lang.blang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlangApplication.class, args);
+	}
+
+}
